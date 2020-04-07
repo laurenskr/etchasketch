afb454eea32d6bdb01a0591bf6bc71374247843a
Laurens Krom <laurens.krom@gmail.com>
2020-04-07 21:46:58 +0200

    build a grid, change colour on hover, added reset function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+//grid maken 
+for (i=0; i < 16; i++) {
+    const container = document.querySelector('#container');
 
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.textContent = 'a';
+    
+    container.appendChild(cell);
+}
+//kleuren bij mouseover
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', (e) => {
+        cell.style.backgroundColor = 'blue';
+    })
+});
+
+//resetfunctie
+function resetCells() {
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = '#eee'
+    })
+}
