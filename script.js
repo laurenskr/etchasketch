b9f98bcc212dc28a1d52b4ffa4e15588fff550ec
Laurens Krom <laurens.krom@gmail.com>
2020-04-07 21:55:37 +0200

    added custom gridSize prompt

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
+let gridSize = 4
+makeGrid(gridSize)
+hoverColor()
 //grid maken 
-for (i=0; i < 16; i++) {
+function makeGrid(gridSize) {
+    for (i=0; i < gridSize**2; i++) {
     const container = document.querySelector('#container');
 
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.textContent = 'a';
+    cell.textContent = '';
     
     container.appendChild(cell);
-}
+    }
+}    
 //kleuren bij mouseover
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
+function hoverColor() {
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
     cell.addEventListener('mouseenter', (e) => {
         cell.style.backgroundColor = 'blue';
     })
 });
+}
 
 //resetfunctie
 function resetCells() {
+    const container = document.querySelector('#container');
     cells.forEach((cell) => {
-        cell.style.backgroundColor = '#eee'
+        // cell.style.backgroundColor = '#eee'
+        container.removeChild(cell);
     })
+    gridSize = prompt("How big should one side of the square be?")
+    makeGrid(gridSize);
+    hoverColor();
 }
